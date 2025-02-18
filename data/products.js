cc2d61daf9a02ca180541d0751f854e4c72c392f
SuperSimpleDev <simon@supersimple.dev>
2025-02-18 20:34:52 +0430

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -126,10 +126,13 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step');
@@ -149,6 +152,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
